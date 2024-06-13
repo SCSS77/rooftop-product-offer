b7a58df961daf93ca9b76c6ee0528b53d377e809
SCSS77 <scastro@emagister.com>
2024-06-13 10:26:33 +0200

    add json-server, move data

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db', 'db.json')); // Asegúrate de que la ruta esté correcta
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on http://localhost:${port}`);
+});
